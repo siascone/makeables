27d49df17bbb827b91be1ebd59de0d0f1b713f94
siascone <spencer.iascone@gmail.com>
2020-04-09 14:37:14 -0700

    add steps controller

--- app/controllers/api/project_steps_controller.rb
+++ app/controllers/api/project_steps_controller.rb
@@ -1,2 +1,51 @@
 class Api::ProjectStepsController < ApplicationController
+
+    def index 
+        @steps = ProjectStep.all
+        render :index
+    end
+
+    def show
+        @step = selected_project_step
+        render :show
+    end
+
+    def create
+        @step = ProjectStep.new(project_step_params)
+        @step.project_id = params[:project_id]
+        if @step.save
+            render :show
+        else
+            render json: @step.errors.full_messages, status: 401
+        end
+    end 
+
+    def update
+        @step = selected_project_step
+        if @step.update(project_step_params)
+            render :show
+        else
+            render json: @step.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @step = selected_project_step
+        if @step
+            @step.destroy
+            render :index
+        else
+            render json: ['Could not find project step'], status: 400
+        end
+    end
+
+    private
+
+    def selected_project_step
+        ProjectStep.find(params[:id])
+    end
+
+    def project_step_params
+        params.require(:step).parmit(:heading, :body)
+    end
 end
