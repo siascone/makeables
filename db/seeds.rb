c77e48d74f433a8d57450c4fc71900ca0daa2784
siascone <spencer.iascone@gmail.com>
2020-04-13 18:12:53 -0700

    add checker table step seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,42 @@ scalloped_frets = Project.create!(user_id: spencer.id, title: "Scallop your Guit
 three_d_printed_snorkel = Project.create!(user_id: spencer.id, title: "3D Printed Snorkel", description: "Snorkeling is sublime. A cheap water activity that makes the incredible world of the ocean available to anyone. It is so wonderful to see young kids surface suddenly after realizing they have seen a fish under them for the first time. Its simplicity of breathing under the ocean surface with a tube masks some of its risks. Sudden Snorkel Death (SSD) -- the loss of life with this activity is surprisingly common. About one death every two weeks here in Hawaii. The common interpretation of why this happens is usually: Snorkeling While Old. But, how do explain a 30 y.o. snorkeling death that occurred last month here? The easiest way to get a feeling for the risks is to actually perform the experiment everyone things of--take a garden hose down as far as you can in the pool and take a breath. I have done this with a lot of people and none can breath in adequately in a little over a foot of water! This is naturally a curve of breathing that starts off normal at the surface and declines rapidly based on body shape and sucking abilities--the human body unlike a snorkeling elephant is very limited. The other obvious problem is the dead space involved in snorkel design. An average human breath is about 500cc. There is about 150 cc of dead space in each breath--the area of your respiratory system that has expired air still stuck in it and must be cleared before fresh air enters the lungs. The average snorkel adds about 180 cc of dead space to this stack and so way over half of your next breath is old stuff and not the oxygen you need. Your breaths get smaller as you go down and contain less oxygen and the urgency to exhale--because holding a breath in is so much harder--shortens the exhalation time, all causing you to breath much more rapidly. What could go wrong?
 
 Snorkels can be designed to get rid of this dead space by the use of valves that separate incoming from outgoing air. This was first mentioned in one of the original patents on a snorkel from 1941. Snorkels with valves are not common so if you want one and have a 3D printer I will show you how to build one. Disclaimer: Build and use at your own risk---if a simple tube can cause your demise imagine what this design could do.",category_id: three_d_printing.id) 
-chess_table = Project.create!(user_id: spencer.id, title: "How to Make a Chess/Checkers Table", category_id: furnature.id)
+chess_table = Project.create!(user_id: spencer.id, title: "How to Make a Chess/Checkers Table", description: "Hi, have I got a fun instructable for your! Today We are making an outdoor checkers/chess table with an old table bottom we found on the side of the road and some plywood leftover from a flooring project.
+
+Note: After the table is finished you can either use existing checker pieces or make your own with a few simple tools.
+
+Supplies:
+Old table base
+
+Scrap plywood big enough for your needs
+
+Tape measure
+
+Compass
+
+String
+
+Pencil and/or marker
+
+1 finishing nail
+
+Hammer
+
+Level - optional
+
+Jig saw
+
+Hand sander
+
+Hole saw - if making your own checkers
+
+Screws
+
+Painters tape
+
+Outdoor Paint - I used red, black, and white for the table and black, white, gold, and silver for the checkers
+
+Paintbrushes", category_id: furnature.id)
 gopro_nosecone = Project.create!(user_id: spencer.id, title: "Model Rocket With GoPro Nose Cone", category_id: science.id)
 halfpipe = Project.create!(user_id: spencer.id, title: "Build a Backyard Halfpipe", category_id: outdoor.id)
 pergola = Project.create!(user_id: spencer.id, title: "The Perfect Pergola", category_id: outdoor.id)
@@ -219,3 +254,105 @@ three_d_step_5 = ProjectStep.create!(heading: "Step 5: Testing", body: "Not much
 Future studies will involve on-water testing under hopefully better controlled conditions.", project_id: three_d_printed_snorkel.id)
 
 three_d_step_6 = ProjectStep.create!(heading: "Step 6: Using It", body: "I went through a lot of design changes in the building of these snorkels. I put together a GIF of some of the design debris that went into the garbage. Starting out with three valves and using commercially available purge valve inserts that can be obtained on the web. These proved to be too restrictive and vibrated with hard use. I then switched to respirator valves that can be bought to simulate CPR--these also proved to be too small and restrictive. The design of these valves incorporates a very large size and a minimum size to the exit tubing to provide good breath feel. This is hard to describe but becomes apparent on trying out the snorkel. It is disconcerting to have that feeling of breathing through a straw underwater. Having valves on snorkels amplifies the perception that its a struggle to get air at depth but having very large valves minimizes this. I also tried a variety of silicon tubing but found its wall thickness and stiffness made it hard to work with. The other problem with valved snorkels involve clearing the inhale tube if you get inadvertent water entry. If this occurs blowing will only clear out the water in the snorkel body but you almost have to suck clear the upper hose to continue. Simple snorkels can be easily cleared by just forcefully blowing out. I just added another Valved Snorkel design on Thingiverse--It places the input valve at the top and allows for better airway protection--also the design is lighter and more elegant: https://www.thingiverse.com/thing:4197780 If you enjoyed this information about snorkels you might look at my other Instructables that deal with them: https://www.instructables.com/id/Snorkel-Sniffer/, https://www.instructables.com/id/Oxygen-Measurement-in-Snorkels/,https://www.instructables.com/id/CO2-Measurement-in-Snorkels/", project_id: three_d_printed_snorkel.id)
+
+chess_step_1 = ProjectStep.create!(heading: "Step 1: Measure the Base - Measure the Wood", body: "The base we had, had a lower ring below the top, so I decided to make a shelf to store a basket for the checker pieces in, under the table when it was finished. Since I needed a lower disk to sit on the ring and a much larger disk for the game table top, I took two measurements.
+
+For the lower disk/shelf I took the measurement then ended up cutting it slightly smaller because of the width of the scrap piece of wood I had to use for it.
+
+For the table top I measured the width between the two hardware attachment sites to get the barest minimum size I needed. The scrap piece I had for the top was large enough to make the top a few inches bigger all the way around.", project_id: chess_table.id)
+
+chess_step_2 = ProjectStep.create!(heading: "Step 2: Draw Your Disk/Circle With a Compass", body: "To use the compass to draw your disk, first make sure you have a pencil, or lead depending on the type of compass you have inserted.
+
+Next, divide the total width of your disk in half to get the radius and this is what you need to set your compass to.
+
+Then, place and hold the point of the compass where you want the center of your disk to be and holding it steady, turn the pencil end of the compass all the way around to completely mark your circle.
+
+Note: I used this method for the smaller disk", project_id: chess_table.id)
+
+chess_step_3 = ProjectStep.create!(heading: "Step 3: Draw Your Disk/Circle With a Piece of Twine and a Marker", body: "To use this method, after you have determined your total desired width for your disk, divide that measurement in half to determine you radius.
+
+Once you have your radius, find your center and tap a finishing nail into the wood just enough to hold a little tension.
+
+Tie one end of your string to the nail.
+
+Then, measure out the length of your radius and tie the other end to your marker or pencil. I used a marker because the rough surface of the plywood kept breaking my lead.
+
+Once both ends are securely tied, keep slight tension and holding the marker upright, draw your circle.
+
+Note: I used this method for the table top, because my compass did not open large enough for my radius. Also, this method works well enough, but since hands are not always consistently, you circle may have a imperfections in it. It is okay.", project_id: chess_table.id)
+
+chess_step_4 = ProjectStep.create!(heading: "Step 4: Cut Out Your Disk(s)", body: "Use your jig saw to cut out your disk or disks, depending on your particular table base and preference.
+
+As you can see, I used two different jig saws. The tension screws on my old one just would not hold the blade in place, so I had to swap out. If you have one that works, that is all you need.
+
+After you cut your disks, check your fit. There should not be a problem as long as your measurements and math are correct.
+
+REMEMBER: Measure twice. Cut once. You can always cut off a little more, but you cannot UN-cut something.", project_id: chess_table.id)
+
+chess_step_5 = ProjectStep.create!(heading: "Step 5: Cut Your Checkers", body: "If you are using existing game pieces, this step can be skipped over.
+
+If you are making your own checkers pieces, your options are only as limited as your imagination and resources. You could paint stones and use them or some other fun idea, but I chose to make some out of the wood scraps I had, using a hole saw, and my cordless drill. If your saw is not very sharp, or if your drill is not very strong, you may want to use a corded drill. One of the teeth on my hole saw was slightly bent so it took more effort to cut so I went through a few batteries before switching to a corded drill.
+
+You will need 12 checkers for each player, so 24 in total.
+
+Note: I cut 34 total, so each player would have 5 back up pieces, just in case something happens to any of them or any come up missing.", project_id: chess_table.id)
+
+chess_step_6 = ProjectStep.create!(heading: "Step 6: Sand, Sand, and Sand", body: "This step is pretty self explanatory. Sand the all the wood stuff, table top and checkers, including edges.
+
+I did not take pictures of sanding the checkers, but I did that by hand with sand paper and for any stubborn little bristly bits around the edge that just did not want to sand smooth, I used nail trimmer to snip them down to avoid future splinters.", project_id: chess_table.id)
+
+chess_step_7 = ProjectStep.create!(heading: "Step 7: Paint and Attach", body: "If only doing a table top, paint the underside only. If you are also doing a piece that will be trapped between the table top and the base, paint the top and bottom of that piece as well.
+
+Once your pieces have dried, lay the table top on the ground with the painted bottom facing up.
+
+If doing a second piece, like mine, place that piece on top of the table top with the bottom facing up.
+
+Then, invert the table onto the table top with the smaller piece in the middle, making sure it is not under one of the attachment sites.
+
+Next, made sure your base is centered on the disk and even all the way around. I eyeballed it, but you could measure if you wanted to be precise.
+
+Then, use screws, and washers if necessary, to attach the base to the table top.
+
+Once the tabletop had been attached, turn the table upright and center the smaller disk.
+
+Note: At this point, rubber feet, foam tape, or something else could be used to hold the bottom disk securely in place if desired. I do not plan to move the table often so I did not do this, because I do not think I will have any problems with it shifting.
+
+", project_id: chess_table.id)
+
+chess_step_8 = ProjectStep.create!(heading: "Step 8: Draw the Grid for Your Checker/Chess Board", body: "The grid for a standard checker/chess board is 8 squares x 8 squares. I did my squares 3 inches in size. Of course you will have to adjust your size based on your table top measurements. As long as your game board is 8 squares x 8 squares, it does not matter how big they are.", project_id: chess_table.id)
+
+chess_step_9 = ProjectStep.create!(heading: "Step 9: Tape and Paint", body: "After drawing my grid/game board, I taped off the boarder and paint-stained the background and table edges, red. If you have never done this before, it is something that I like to do, I use paint like stain. I dip a rag into the paint and rub it on the surface that I am wanting to paint. It gives it a nice smooth even coat.
+
+Once the background is painted and dried, tape off your grid. It is your choice how you do this. You will figure out your own flow as you go. Once you have it all taped pick one of your game board colors and paint every other square that color.
+
+After you have painted all the exposed squares, carefully remove the tape, before it dries.
+
+Once dried, re-tape to expose new squares to paint.
+
+After all squares of one color have been painted and dried, repeat the process with the second color.
+
+Note: While painting the first taped sections, I also went into the areas that were blocked off with the tape and marked the rest of the squares that would be the same color. This help prevent me from getting confused and painting the wrong square the wrong color. As you can see in the pictures, when it is all taped up it makes it look crazy and confusing a little, because you end up with small rectangles of different sizes going different directions and you can easily get confused.", project_id: chess_table.id)
+
+chess_step_10 = ProjectStep.create!(heading: "Step 10: Fix the Bleed", body: "If your plywood was not completely smooth, mine was not, then like me, you have have some bleed where the paint worked its way under the edge of the tape, or you may have just accidentally dropped or smudged some paint on top of another color. No big deal. Paint is forgiving.
+
+Touch up any bleed with a fine-detail paintbrush, and any smudges or spills with an appropriate sized brush and needed color for correction.
+
+Note:I did not re-tape, as I felt it would yield the same results I was trying to fix.", project_id: chess_table.id)
+
+chess_step_11 = ProjectStep.create!(heading: "Step 11: Paint Your Checkers", body: "This step is also pretty self-explanatory.
+
+Separate your checkers in to two piles of equal number.
+
+Paint half the checker in one color.
+
+Paint the remaining checker in the second color.
+
+After the checkers are dry, paint a crown on one of the sides of each checker piece.
+
+Note: You can paint all the crowns the same color if you wish. I chose to paint the crowns different colors for each player/team. I painted silver crowns on the black pieces and gold crowns on the white pieces, for better contrast. Also, you can use a stencil if you wish; I just free-handed mine.", project_id: chess_table.id)
+
+chess_step_12 = ProjectStep.create!(heading: "Step 12: Time to Win, Er, Play ;-P", body: "Now that your game table is all nice and beautiful and complete, it is time to get your game on!
+
+I bought a red basket from the dollar store to keep all my checker pieces in so I can store them on the shelf under the table top. It keeps them tidy, easily accessible, and out of the way.
+
+I hope you enjoyed this instructable and find a table base on the side of the road real soon, so you can try it out and make your own AWESOME Outdoor Checkers and Chess game table.", project_id: chess_table.id)
