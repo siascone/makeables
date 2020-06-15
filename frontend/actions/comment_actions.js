fe52fd4d494fb88bf79c84efbf90b8b07dff74b3
siascone <spencer.iascone@gmail.com>
2020-06-15 14:50:24 -0700

    add comment_actions

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -1,2 +1,55 @@
 import React from 'react';
-import * as CommentApiUtil from '../util/comments_api_util';
+import * as CommentsApiUtil from '../util/comments_api_util';
+
+export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+const receiveAllComments = (comments) => ({
+    type: RECEIVE_ALL_COMMENTS,
+    comments
+});
+const reciveComment = (comment) => {
+    return {
+        type: RECEIVE_COMMENT,
+        comment
+    }
+};
+const removeComment = (commentId) => ({
+    type: REMOVE_COMMENT,
+    commentId
+});
+const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors
+});
+const clearErrors = () => ({
+    type: CLEAR_ERRORS
+});
+
+export const fetchALLComments = () => dispatch => {
+    return CommentsApiUtil.fetchAllComments()
+        .then((comments) => dispatch(receiveAllComments(comments)));
+};
+
+export const fetchComment = (commentId) => dispatch => {
+    return CommentsApiUtil.fetchComment(commentId)
+        .then((comment) => dispatch(reciveComment(comment)));
+};
+
+export const createComment = (comment) => dispatch => {
+    return CommentsApiUtil.createComment(comment)
+        .then(res => {
+            dispatch(reciveComment(res))
+            return res.comment
+        }, (errors) => {
+            dispatch(receiveErrors(errors.responseJSON))
+        });
+};
+
+export const deleteComment = (commentID) => dispatch => {
+    return CommentsApiUtil.deleteComment(commentId)
+        .then((comment) = dispatch(removeComment(comment.id)));
+};
