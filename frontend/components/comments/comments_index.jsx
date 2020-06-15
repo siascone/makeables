adcd7d37d004d022b230370190f653384c4a8616
siascone <spencer.iascone@gmail.com>
2020-06-15 16:08:00 -0700

    add comments index

--- frontend/components/comments/comments_index.jsx
+++ frontend/components/comments/comments_index.jsx
@@ -1,3 +1,31 @@
 import React from 'react';
 import CommentIndexItem from './comment_index_item';
 
+class CommentsIndex extends React.Component {
+    componentDidMount() {
+        this.props.fetchAllComments();
+    }
+
+    render() {
+        if (this.props.comments.length <= 0) {
+            return null;
+        }
+
+        const { comments, users } = this.props;
+        return (
+            <div className='comment-index-main'>
+                {
+                    comments.map((comment, idx) =(
+                        <CommentIndexItem
+                            comment={comment}
+                            user={users[comment.user_id]}
+                            key={idx}
+                        />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default CommentsIndex
